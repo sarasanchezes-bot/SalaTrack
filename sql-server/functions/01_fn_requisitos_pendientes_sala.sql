f0dd6bf20b955f54d50dfaf356cb3486f97853ec
Pedro Mclean <pedro.mcleanwa@amigo.edu.co>
2026-08-30 19:36:16 -0600

    fix: corregir logica de fn_requisitos_pendientes_sala usando RequisitoCurso y AsignacionSemestral

--- sql-server/functions/01_fn_requisitos_pendientes_sala.sql
+++ sql-server/functions/01_fn_requisitos_pendientes_sala.sql
@@ -1,11 +1,7 @@
 USE SalaTrack;
 GO
 
-IF OBJECT_ID('dbo.fn_requisitos_pendientes_sala', 'FN') IS NOT NULL
-    DROP FUNCTION dbo.fn_requisitos_pendientes_sala;
-GO
-
-CREATE FUNCTION dbo.fn_requisitos_pendientes_sala (
+CREATE OR ALTER FUNCTION dbo.fn_requisitos_pendientes_sala (
     @sala_id INT
 )
 RETURNS TABLE
@@ -13,12 +9,25 @@ AS
 RETURN
 (
     SELECT 
-        r.requisito_id,
-        r.descripcion,
-        r.estado,
-        r.fecha_solicitud
-    FROM Requisito r
-    WHERE r.sala_id = @sala_id
-      AND r.estado = 'Pendiente'
+        c.curso_id,
+        c.nombre AS curso,
+        s.software_id,
+        s.nombre AS software_faltante,
+        rc.nivel_permiso_necesario
+    FROM AsignacionSemestral a
+    JOIN Curso c ON c.curso_id = a.curso_id
+    JOIN RequisitoCurso rc ON rc.curso_id = a.curso_id
+        AND rc.es_obligatorio = 1
+        AND rc.software_id IS NOT NULL
+    JOIN Software s ON s.software_id = rc.software_id
+    WHERE a.sala_id = @sala_id
+      AND a.estado <> 'cancelada'
+      AND NOT EXISTS (
+          SELECT 1
+          FROM EquipoSoftware es
+          JOIN Equipo e ON e.equipo_id = es.equipo_id
+          WHERE e.sala_id = a.sala_id
+            AND es.software_id = rc.software_id
+      )
 );
 GO
